5fad6ef680ec5f5e77851e4ae7341341104df72f
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-12-16 14:07:30 +0100

    feat: added card with dynamic content and button styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
 import languages from "./data/languages";
+import { useState } from "react";
 
 function App() {
+
+  const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
 
   return (
     <>
       <header>
         <h1>Learn Web Developement</h1>
       </header>
+
       <ul className="container row">
         {languages.map((curLanguage) => {
-          console.log(curLanguage);
           return (
             <li key={curLanguage.id}>
-              <button>{curLanguage.title}</button>
+              <button 
+                onClick={() => setSelectedLanguage(curLanguage)}
+                className={selectedLanguage.id === curLanguage.id ? "active" : ""}
+              >
+                {curLanguage.title}
+              </button>
             </li>
           )
         })}
       </ul>
 
-      
+      <div className="container card">
+        <h2>{selectedLanguage.title}</h2>
+        <p>{selectedLanguage.description}</p>
+      </div>
     </>
   )
 }
